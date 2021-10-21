9de8ca691e04699dbd75eb10d2b1f1a08279a9d2
Anastasia-Alt167 <85153085+Anastasia-Alt167@users.noreply.github.com>
2021-10-21 15:04:25 +0300

    Refract2

--- AddAdmin.xaml.cs
+++ AddAdmin.xaml.cs
@@ -26,138 +26,163 @@ namespace Kursovoy
         {
             public static Flights flightsRecord;
         }
-    //    public AddAdmin()
-    //    {
-    //        InitializeComponent();
-    //        using (var context = new AviakompaniyaEntities())
-    //        {
-    //            //Flight_Number.Text = context.Flights.Where(x => x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number).Select(x => x.Flight_Number).FirstOrDefault().ToString();
-    //            Fly_Date.Text = context.Flights.Where(x => x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number).Select(x => x.Fly_Date).FirstOrDefault()?.ToString();
-    //            Fly_Time.Text = context.Flights.Where(x => x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number).Select(x => x.Fly_Time).FirstOrDefault()?.ToString();
-    //            Fly_Town.Text = context.Flights.Where(x => x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number).Select(x => x.Fly_Town).FirstOrDefault()?.ToString();
-    //            Arrival_Date.Text = context.Flights.Where(x => x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number).Select(x => x.Arrival_Date).FirstOrDefault()?.ToString();
-    //            Arrival_Town.Text = context.Flights.Where(x => x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number).Select(x => x.Arrival_Town).FirstOrDefault()?.ToString();
-    //            Arrival_Time.Text = context.Flights.Where(x => x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number).Select(x => x.Arrival_Time).FirstOrDefault()?.ToString();
-    //            Aircraft_Number.Text = context.Flights.Where(x => x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number).Select(x => x.Aircraft_Number).FirstOrDefault()?.ToString();
-    //        }
-    //    }
-    //    const int lengthOfFlightNumber = 10;
-    //    const int lengthOfFlyDate = 10;
-    //    const int lengthOfFlyTime = 5;
-    //    const int lengthOfFlyTown = 40;
-    //    const int lengthOfArrivalDate = 10;
-    //    const int lengthOfArrivalTime = 5;
-    //    const int lengthOfArrivalTown = 40;
-    //    const int lengthOfAircraftNumber = 10;
-    //    public bool CheckFlightsInformation(string stringToCheck) => (stringToCheck.Length < lengthOfFlightNumber) && stringToCheck.All(c => Char.IsLetter(c));
+        public AddAdmin()
+        {
+            InitializeComponent();
+            using (var context = new AviakompaniyaEntities())
+            {
+                //Flight_Number.Text = context.Flights.Where(x => x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number).Select(x => x.Flight_Number).FirstOrDefault().ToString();
+                Fly_Date.Text = context.Flights.Where(x => x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number).Select(x => x.Fly_Date).FirstOrDefault()?.ToString();
+                Fly_Time.Text = context.Flights.Where(x => x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number).Select(x => x.Fly_Time).FirstOrDefault()?.ToString();
+                Fly_Town.Text = context.Flights.Where(x => x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number).Select(x => x.Fly_Town).FirstOrDefault()?.ToString();
+                Arrival_Date.Text = context.Flights.Where(x => x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number).Select(x => x.Arrival_Date).FirstOrDefault()?.ToString();
+                Arrival_Town.Text = context.Flights.Where(x => x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number).Select(x => x.Arrival_Town).FirstOrDefault()?.ToString();
+                Arrival_Time.Text = context.Flights.Where(x => x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number).Select(x => x.Arrival_Time).FirstOrDefault()?.ToString();
+                Aircraft_Number.Text = context.Flights.Where(x => x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number).Select(x => x.Aircraft_Number).FirstOrDefault()?.ToString();
+            }
+            using (var context = new AviakompaniyaEntities())
+            {
+                Fly_Town.ItemsSource = (from t in context.Town
+                                                join f in context.Flights on t.Town_Code equals f.Fly_Town
+                                                select new { t.Town1, f.Fly_Town }).ToList();//context.Town.ToList();
+                //(from t in context.Town
+                // join f in context.Flights on t.Town_Code equals f.Fly_Town
+                // select new { t.Town1, f.Fly_Town }).ToList();
+            }
+        }
+        const int lengthOfFlightNumber = 10;
+        const int lengthOfFlyDate = 10;
+        const int lengthOfFlyTime = 5;
+        const int lengthOfFlyTown = 40;
+        const int lengthOfArrivalDate = 10;
+        const int lengthOfArrivalTime = 5;
+        const int lengthOfArrivalTown = 40;
+        const int lengthOfAircraftNumber = 10;
+        public bool CheckFlightsInformation(string stringToCheck) => (stringToCheck.Length < lengthOfFlightNumber) && stringToCheck.All(c => Char.IsLetter(c));
+        private void Fly_Town_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
 
-       private void BtnSave_Click(object sender, RoutedEventArgs e)
-       {
-    //        //if (!(CheckFlightsInformation(Flight_Number.Text)))
-    //        //{
-    //        //    Flight_Number.ToolTip = "Некорректно введен номер самолета.";
-    //        //    var backgroundColor = new BrushConverter();
-    //        //    Flight_Number.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
-    //        //}
-    //        //else
-    //        //{
-    //        //    Flight_Number.Background = Brushes.Transparent;
-    //        //    Flight_Number.ToolTip = null;
-    //        //}
-    //        if (!(CheckFlightsInformation(Fly_Date.Text)))
-    //        {
-    //            Fly_Date.ToolTip = "Некорректно введена дата вылета.";
-    //            var backgroundColor = new BrushConverter();
-    //            Fly_Date.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
-    //        }
-    //        else
-    //        {
-    //            Fly_Date.Background = Brushes.Transparent;
-    //            Fly_Date.ToolTip = null;
-    //        }
-    //        if (!(CheckFlightsInformation(Fly_Time.Text)))
-    //        {
-    //            Fly_Time.ToolTip = "Некорректно введено время вылета.";
-    //            var backgroundColor = new BrushConverter();
-    //            Fly_Time.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
-    //        }
-    //        else
-    //        {
-    //            Fly_Time.Background = Brushes.Transparent;
-    //            Fly_Time.ToolTip = null;
-    //        }
-    //        if (!(CheckFlightsInformation(Fly_Town.Text)))
-    //        {
-    //            Fly_Town.ToolTip = "Некорректно введен город вылета";
-    //            var backgroundColor = new BrushConverter();
-    //            Fly_Town.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
-    //        }
-    //        else
-    //        {
-    //            Fly_Town.Background = Brushes.Transparent;
-    //            Fly_Town.ToolTip = null;
-    //        }
-    //        if (!(CheckFlightsInformation(Arrival_Date.Text)))
-    //        {
-    //            Arrival_Date.ToolTip = "Некорректно введена дата прилета.";
-    //            var backgroundColor = new BrushConverter();
-    //            Arrival_Date.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
-    //        }
-    //        else
-    //        {
-    //            Arrival_Date.Background = Brushes.Transparent;
-    //            Arrival_Date.ToolTip = null;
-    //        }
-    //        if (!(CheckFlightsInformation(Arrival_Town.Text)))
-    //        {
-    //            Arrival_Town.ToolTip = "Некорректно введен город прилета.";
-    //            var backgroundColor = new BrushConverter();
-    //            Arrival_Town.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
-    //        }
-    //        else
-    //        {
-    //            Arrival_Town.Background = Brushes.Transparent;
-    //            Arrival_Town.ToolTip = null;
-    //        }
-    //        if (!(CheckFlightsInformation(Arrival_Time.Text)))
-    //        {
-    //            Arrival_Time.ToolTip = "Некорректно введено время прилета.";
-    //            var backgroundColor = new BrushConverter();
-    //            Arrival_Time.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
-    //        }
-    //        else
-    //        {
-    //            Arrival_Time.Background = Brushes.Transparent;
-    //            Arrival_Time.ToolTip = null;
-    //        }
-    //        if (!(CheckFlightsInformation(Aircraft_Number.Text)))
-    //        {
-    //            Aircraft_Number.ToolTip = "Некорректно введен номер самолета.";
-    //            var backgroundColor = new BrushConverter();
-    //            Aircraft_Number.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
-    //        }
-    //        else
-    //        {
-    //            Aircraft_Number.Background = Brushes.Transparent;
-    //            Aircraft_Number.ToolTip = null;
-    //            using (var context = new AviakompaniyaEntities())
-    //            {
+            using (var db = new AviakompaniyaEntities())
+            {
+                NameOfCity p = new NameOfCity(db.Town.FirstOrDefault(x => x.Town_Code == (int)((ComboBox)sender).SelectedValue));
+                if (Fly_Town.SelectedIndex != -1)
+                {
 
-    //                //(from x in context.Flights where x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number select x).ToList().ForEach(x => x.Flight_Number = Flight_Number.Text);
-    //                (from x in context.Flights where x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number select x).ToList().ForEach(x => x.Fly_Date = Fly_Date.Text);
-    //                (from x in context.Flights where x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number select x).ToList().ForEach(x => x.Fly_Time = Fly_Time.Text);
-    //                (from x in context.Flights where x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number select x).ToList().ForEach(x => x.Fly_Town = Fly_Town.Text);
-    //                (from x in context.Flights where x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number select x).ToList().ForEach(x => x.Arrival_Date = Arrival_Date.Text);
-    //                (from x in context.Flights where x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number select x).ToList().ForEach(x => x.Arrival_Town = Arrival_Town.Text);
-    //                (from x in context.Flights where x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number select x).ToList().ForEach(x => x.Arrival_Time = Arrival_Time.Text);
-    //                (from x in context.Flights where x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number select x).ToList().ForEach(x => x.Aircraft_Number = Aircraft_Number.Text);
-    //                context.SaveChanges();
-    //            }
-    //        }
-       }
+                    Fly_Town.ItemsSource = db.Flights.Where(x => x.Fly_Town == (int)((ComboBox)sender).SelectedValue).Select(x => new { x.Town.Town1, x.Arrival_Town }).ToList();
+
+                    Fly_Town.SelectedIndex = -1;
+                }
+
+            }
+        }
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            //if (!(CheckFlightsInformation(Flight_Number.Text)))
+            //{
+            //    Flight_Number.ToolTip = "Некорректно введен номер самолета.";
+            //    var backgroundColor = new BrushConverter();
+            //    Flight_Number.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
+            //}
+            //else
+            //{
+            //    Flight_Number.Background = Brushes.Transparent;
+            //    Flight_Number.ToolTip = null;
+            //}
+            if (!(CheckFlightsInformation(Fly_Date.Text)))
+            {
+                Fly_Date.ToolTip = "Некорректно введена дата вылета.";
+                var backgroundColor = new BrushConverter();
+                Fly_Date.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
+            }
+            else
+            {
+                Fly_Date.Background = Brushes.Transparent;
+                Fly_Date.ToolTip = null;
+            }
+            if (!(CheckFlightsInformation(Fly_Time.Text)))
+            {
+                Fly_Time.ToolTip = "Некорректно введено время вылета.";
+                var backgroundColor = new BrushConverter();
+                Fly_Time.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
+            }
+            else
+            {
+                Fly_Time.Background = Brushes.Transparent;
+                Fly_Time.ToolTip = null;
+            }
+            if (!(CheckFlightsInformation(Fly_Town.Text)))
+            {
+                Fly_Town.ToolTip = "Некорректно введен город вылета";
+                var backgroundColor = new BrushConverter();
+                Fly_Town.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
+            }
+            else
+            {
+                Fly_Town.Background = Brushes.Transparent;
+                Fly_Town.ToolTip = null;
+            }
+            if (!(CheckFlightsInformation(Arrival_Date.Text)))
+            {
+                Arrival_Date.ToolTip = "Некорректно введена дата прилета.";
+                var backgroundColor = new BrushConverter();
+                Arrival_Date.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
+            }
+            else
+            {
+                Arrival_Date.Background = Brushes.Transparent;
+                Arrival_Date.ToolTip = null;
+            }
+            if (!(CheckFlightsInformation(Arrival_Town.Text)))
+            {
+                Arrival_Town.ToolTip = "Некорректно введен город прилета.";
+                var backgroundColor = new BrushConverter();
+                Arrival_Town.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
+            }
+            else
+            {
+                Arrival_Town.Background = Brushes.Transparent;
+                Arrival_Town.ToolTip = null;
+            }
+            if (!(CheckFlightsInformation(Arrival_Time.Text)))
+            {
+                Arrival_Time.ToolTip = "Некорректно введено время прилета.";
+                var backgroundColor = new BrushConverter();
+                Arrival_Time.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
+            }
+            else
+            {
+                Arrival_Time.Background = Brushes.Transparent;
+                Arrival_Time.ToolTip = null;
+            }
+            if (!(CheckFlightsInformation(Aircraft_Number.Text)))
+            {
+                Aircraft_Number.ToolTip = "Некорректно введен номер самолета.";
+                var backgroundColor = new BrushConverter();
+                Aircraft_Number.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
+            }
+            else
+            {
+                Aircraft_Number.Background = Brushes.Transparent;
+                Aircraft_Number.ToolTip = null;
+                using (var context = new AviakompaniyaEntities())
+                {
+
+                    //(from x in context.Flights where x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number select x).ToList().ForEach(x => x.Flight_Number = Flight_Number.Text);
+                     (from x in context.Flights where x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number select x).ToList().ForEach(x => x.Fly_Date = Fly_Date.Text);
+                    (from x in context.Flights where x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number select x).ToList().ForEach(x => x.Fly_Time = Fly_Time.Text);
+                    (from x in context.Flights where x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number select x).ToList().ForEach(x => x.Fly_Town = Fly_Town.Text);
+                    (from x in context.Flights where x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number select x).ToList().ForEach(x => x.Arrival_Date = Arrival_Date.Text);
+                    (from x in context.Flights where x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number select x).ToList().ForEach(x => x.Arrival_Town = Arrival_Town.Text);
+                    (from x in context.Flights where x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number select x).ToList().ForEach(x => x.Arrival_Time = Arrival_Time.Text);
+                    (from x in context.Flights where x.Flight_Number == FlightsRecord.flightsRecord.Flight_Number select x).ToList().ForEach(x => x.Aircraft_Number = Aircraft_Number.Text);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+       
     }
 }
-        
 
 
 
@@ -180,4 +205,4 @@ namespace Kursovoy
 
 
 
-            
+
